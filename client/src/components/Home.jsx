78a43cffe1bf5c1fcec91feaa7fc8633c8883194
VISHALKANNAN070 <vishalkannan070@gmail.com>
2025-05-08 21:24:27 +0530

    Revert "title change"
    This reverts commit abbf38f6ff0e9bfb396c7e91b7bd509bc58150ba.

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -36,63 +36,56 @@ const Home = ({
           Search
         </button>
       </form>
-      {loading ? (
-        <div className="text-[#646464] mt-4 text-4xl flex justify-center">
-          Loading songs...
-        </div>
-      ) : (
-        <div>
-          <h1 className="text-2xl font-bold text-white mb-8">Results</h1>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-            {songs.map((song) => (
-              <div
-                onClick={() => onPlaySong(song)}
-                key={song.id}
-                className="bg-[#232323] rounded-lg shadow-md flex items-center p-4 transition"
-              >
-                <img
-                  src={song.cover_m}
-                  alt={song.title}
-                  className="w-16 h-16 rounded shadow-md object-cover mr-4"
-                />
-                <div className="flex-1">
-                  <h2 className="text-white text-lg font-semibold truncate">
-                    {song.title.length > 15
-                      ? song.title.slice(0, 15) + "..."
-                      : song.title}{" "}
-                  </h2>
-                  <p className="text-gray-400 truncate">{song.artist}</p>
-                  <div className="mt-2 flex gap-2">
-                    <button
-                      onClick={(e) => {
-                        e.stopPropagation();
-                        onToggleFavourite(song);
-                      }}
-                      className={`text-sm px-3 py-1 rounded font-bold focus:outline-none flex items-center ${
-                        favourites.find((s) => s.id === song.id)
-                          ? "bg-[#232323]"
-                          : "bg-[#232323]"
-                      }`}
-                      title="Toggle favourite"
-                    >
-                      {favourites.find((s) => s.id === song.id) ? (
-                        <FaHeart className="text-pink-500" />
-                      ) : (
-                        <FaHeart className="text-gray-200" />
-                      )}
-                    </button>
-                  </div>
-                </div>
+    {loading ? (<div className="text-[#646464] mt-4 text-4xl flex justify-center">Loading songs...</div>) : (<div>
+      <h1 className="text-2xl font-bold text-white mb-8">Results</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {songs.map((song) => (
+          <div
+            onClick={() => onPlaySong(song)}
+            key={song.id}
+            className="bg-[#232323] rounded-lg shadow-md flex items-center p-4 transition"
+          >
+            <img
+              src={song.cover_m}
+              alt={song.title}
+              className="w-16 h-16 rounded shadow-md object-cover mr-4"
+            />
+            <div className="flex-1">
+              <h2 className="text-white text-lg font-semibold truncate">
+                {song.title}
+              </h2>
+              <p className="text-gray-400 truncate">{song.artist}</p>
+              <div className="mt-2 flex gap-2">
+                <button
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    onToggleFavourite(song);
+                  }}
+
+                  className={`text-sm px-3 py-1 rounded font-bold focus:outline-none flex items-center ${
+                    favourites.find((s) => s.id === song.id)
+                      ? "bg-[#232323]"
+                      : "bg-[#232323]"
+                  }`}
+                  title="Toggle favourite"
+                >
+                  {favourites.find((s) => s.id === song.id) ? (
+                    <FaHeart className="text-pink-500" />
+                  ) : (
+                    <FaHeart className="text-gray-200" />
+                  )}
+                </button>
               </div>
-            ))}
-          </div>
-          {songs.length === 0 && (
-            <div className="mt-10 text-gray-400 text-center">
-              Search for songs to begin!
             </div>
-          )}
+          </div>
+        ))}
+      </div>
+      {songs.length === 0 && (
+        <div className="mt-10 text-gray-400 text-center">
+          Search for songs to begin!
         </div>
       )}
+          </div>)}
     </section>
   );
 };
